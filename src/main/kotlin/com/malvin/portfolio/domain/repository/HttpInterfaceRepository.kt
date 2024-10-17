71fe7728438e8c41764032aa17281cfcef838491
malvin222 <mahebin@naver.com>
2024-10-17 16:40:35 +0900

    대시보드 개발 - 컨트롤러, 서비스, DTO

--- src/main/kotlin/com/malvin/portfolio/domain/repository/HttpInterfaceRepository.kt
+++ src/main/kotlin/com/malvin/portfolio/domain/repository/HttpInterfaceRepository.kt
@@ -2,10 +2,9 @@ package com.malvin.portfolio.domain.repository
 
 import com.malvin.portfolio.domain.entity.HttpInterface
 import org.springframework.data.jpa.repository.JpaRepository
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 interface HttpInterfaceRepository : JpaRepository<HttpInterface, Long> {
 
-    fun countAllByCreatedDateTimeBetween(start: LocalDate, end: LocalDateTime)
+    fun countAllByCreatedDateTimeBetween(start: LocalDateTime, end: LocalDateTime): Long
 }
